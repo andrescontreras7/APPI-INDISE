a64c39ed74d6cd1011d0da4e4799e8f84cdab801
Andres cavadia <andrescavadiaaa@gmail.com>
2024-06-20 08:00:03 -0500

    controladores para estadisticas

--- routes/useLogin.routes.js
+++ routes/useLogin.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const RouterLogin = express.Router();
-const { getUserActivity } = require('../controller/userLogin.controller');
+const { getUserActivity, getUserActivityById } = require('../controller/userLogin.controller');
 
 
 RouterLogin.get('/user_activity', getUserActivity);
+RouterLogin.get('/user_activityBy/:id', getUserActivityById);
 
 module.exports = RouterLogin;
